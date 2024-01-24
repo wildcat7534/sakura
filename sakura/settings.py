0de5346897f8472703eff0a1bd9eb20863b4900f
clement <wildcat6@gmail.com>
2024-01-24 12:18:44 +0100

    suite

--- sakura/settings.py
+++ sakura/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'sakuraOllama.apps.SakuraollamaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- sakuraOllama/models.py
+++ sakuraOllama/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+import datetime
+from django.utils import timezone
+import ollama from 'ollama'
 
 # Create your models here.
+
