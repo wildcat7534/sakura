c9ec42ebf4f0a5ace34b17323422c31f44038329
clement <wildcat6@gmail.com>
2024-01-25 14:10:23 +0100

    ça marche

--- sakuraOllama/views.py
+++ sakuraOllama/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, httpRequest
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
 
@@ -7,5 +7,12 @@ from django.views import generic
 # Create your views here.
 
 def sakura(request):
-  title = 'Sakura Ollama'
-  return render(request, 'sakuraOllama/sakura.html', {'page_title': title})
+  title = 'Sakura'
+  chatStream = ""
+  sakuraHtml = loader.get_template('sakuraOllama/sakura.html')
+
+  if request.method == 'POST':
+    # Do something
+    chatStream = request.POST['chatStream']
+    return HttpResponse(chatStream)
+  return render(request, 'sakuraOllama/sakura.html', {'page_title': title, 'chatStream': chatStream})
