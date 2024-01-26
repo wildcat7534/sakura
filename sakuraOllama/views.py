acc5dfac570ae2f14c700864d361c09c8049f2b4
clement <wildcat6@gmail.com>
2024-01-26 11:51:23 +0100

    sakura is alive over the net

--- sakuraOllama/views.py
+++ sakuraOllama/views.py
@@ -5,20 +5,42 @@ import json
 
 from django.shortcuts import render
 from django.views import generic
-# Create your views here.
+
+import requests
+import ollama
 
 def sakura(request):
+
   title = 'Sakura'
   chatBody = "attente"
 
   if request.method == 'POST':
-    # Do something
-    print("POST request", request.POST)
        
-    chatBody = request.POST.get('text')
+    chatBody = request.POST.get('question')
         
     print("chatBody: ", chatBody)
-    return render(request, 'sakuraOllama/sakura.html', {'page_title': title, 'chatStream': chatBody})
+
+    data = {'model': 'sakura', 'prompt': 'why the sky is blue'}
+    #post the server ollama with the data and with the -d option in the paramaters ?
+
+    ollama_response = requests.post('http://localhost:11434/api/generate', data=data )
+    print(ollama_response)
+
+    # response = ollama.chat(model='sakura', messages=[
+    #  {
+    #   'role': 'user',
+    #   'content': chatBody,
+    # },
+    # ])
+    # chatBody = response['message']['content']
+
+    #print(ollama_response)
+
+
+  return render(request, 'sakuraOllama/sakura.html', {'page_title': title, 'chatStream': chatBody})
+
+
+
     # message = json.loads(chatBody)['message']
     # chatStream = request.POST['chatStream']
     # chatStream2 = request.parse_body()
