60040428b2871f7639e3c1b99d68ccedad53fef1
wildcat7534 <wildcat6@gmail.com>
2024-02-03 21:21:26 +0100

    Update STATIC_URL in settings.py

--- sakura/settings.py
+++ sakura/settings.py
@@ -121,7 +121,7 @@ STATIC_ROOT = "/home/wildcat/sakura/sakuraOllama/static"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
