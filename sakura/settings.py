b3269cd24c66ab94d266de7b659d4b0b5f088ecc
wildcat7534 <wildcat6@gmail.com>
2024-02-03 18:27:51 +0100

    Add STATIC_ROOT path to settings.py

--- sakura/settings.py
+++ sakura/settings.py
@@ -113,6 +113,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_ROOT = "/home/wildcat/sakura/sakuraOllama/static"
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
