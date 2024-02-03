1c858c3a64cf28b5890af351040fa6b7436f3595
wildcat7534 <wildcat6@gmail.com>
2024-02-03 21:02:10 +0100

    Update ALLOWED_HOSTS in settings.py

--- sakura/settings.py
+++ sakura/settings.py
@@ -27,7 +27,7 @@ INTERNAL_IPS = ["wildcat.hd.free.fr"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
