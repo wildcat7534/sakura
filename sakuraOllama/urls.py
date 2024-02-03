656fb2abe4a2833c3a8f38d975a2c6c40c48ddad
wildcat7534 <wildcat6@gmail.com>
2024-02-03 18:34:29 +0100

    Remove unnecessary URL paths

--- sakuraOllama/urls.py
+++ sakuraOllama/urls.py
@@ -5,7 +5,4 @@ app_name = 'sakuraOllama'
 
 urlpatterns = [
   path('', views.sakuraAsync, name='sakuraasync'),
-"""   path('sakuravllm', views.sakuraVllm, name='sakuravllm'),
-  path('sakuraTransormers', views.sakuraTransormers, name='sakuraTransormers'),
-  path('sakuraasync', views.sakuraAsync, name='sakuraasync'), """
 ]
