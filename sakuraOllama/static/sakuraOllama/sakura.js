785b15664fc32bc7b78b993216dd9b70a2e152a5
wildcat7534 <wildcat6@gmail.com>
2024-02-03 17:47:45 +0100

    up scroll et ordre requierements.txt

--- sakuraOllama/static/sakuraOllama/sakura.js
+++ sakuraOllama/static/sakuraOllama/sakura.js
@@ -1,11 +1,4 @@
-// import ollama from 'ollama'
 
-// const response = await ollama.chat({
-//   model: 'mistral',
-//   messages: [{ role: 'user', content: 'Pourquoi le ciel est bleu ?' }],
-// })
-// console.log(response.message.content)
-
+// Go to the bottom of the chat
 const chatStreamContainer = document.querySelector('#chatStreamContainer');
-
-windows.scrollTo(0, chatStreamContainer.scrollHeight, behavior = 'smooth', block = 'end', inline = 'nearest');
+chatStreamContainer.scrollTo(0, chatStreamContainer.scrollHeight, behavior = 'smooth' , block = 'end', inline = 'nearest');
--- sakuraOllama/urls.py
+++ sakuraOllama/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'sakuraOllama'
 
 urlpatterns = [
-  path('', views.sakura, name='sakura'),
+  path('', views.sakuraAsync, name='sakuraasync'),
   path('sakuravllm', views.sakuraVllm, name='sakuravllm'),
   path('sakuraTransormers', views.sakuraTransormers, name='sakuraTransormers'),
   path('sakuraasync', views.sakuraAsync, name='sakuraasync'),
