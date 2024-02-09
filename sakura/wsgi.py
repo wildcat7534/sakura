2207d8758c39eb4ee19583c0501d5b042082282c
WiLDCaT <wildcat6@gmail.com>
2024-02-09 18:38:55 +0100

    Update sys.path in wsgi.py

--- sakura/wsgi.py
+++ sakura/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 import os, sys
 
-sys.path.append('/var/www/sakura')
+sys.path.append('/home/wildcat/venvs/djangoSakuraVenv/chatAI')
 
 from django.core.wsgi import get_wsgi_application
 
